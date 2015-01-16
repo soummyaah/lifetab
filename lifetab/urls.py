55b8dcf9f6804266cbfd55a08d2fa582f1d79188
anjali2906 <ujjainia.2906@gmail.com>
2015-01-17 04:38:59 +0530

    All the UI is done

--- lifetab/urls.py
+++ lifetab/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lifetab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-     url(r'^$', 'lifetab.views.home', name='home'),
+    url(r'^$', 'lifetab.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^todo/', include('todos.urls', namespace='todo')),
     url(r'^journal/', include('journal.urls', namespace='journal')),
