5cecfe29b5b1802b5b1032b278bb82b1d8ce246a
Shivam Chaturvedi <devs4v@gmail.com>
2015-01-16 19:23:26 +0530

    Added basic models for todos and journal

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+class Entry(TimeStampedModel):
+	FEELING_CHOICES = (
+		('HAPP', "happy"),
+		('SADD', "sad"),
+		('NOST', "nostalgic"),
+		('CURI', "curious"),
+		('WOND', "wonderful"),
+	)
+
+	is_protected = models.BooleanField(default=False)
+	title = models.CharField(max_length=2000)
+	content = models.CharField(max_length=100000)
+	
+	feeling = models.ChoiceField(max_length=4,
+								choices=FEELING_CHOICES,
+								default='HAPP')
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+class Todo(TimeStampedModel):
+	title = models.CharField(max_length=1000)
+	notes = models.CharField(max_length=5000)
+
+	due = models.DateTimeField(auto_now=True)
