a356c959469a9fd9c1e1de9586f13e4e0ae96b95
Shivam Chaturvedi <devs4v@gmail.com>
2015-01-24 18:39:43 +0530

    migrated db to remove done field from todo model

---
+++ todos/migrations/0004_remove_todo_done.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('todos', '0003_remove_todo_archived'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='todo',
+            name='done',
+        ),
+    ]
